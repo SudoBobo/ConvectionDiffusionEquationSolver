9849e56c83a50131b1f085fcfffb954f948004bd
SudoBobo <ivan.koptelov@phystech.edu>
2016-09-06 20:52:50 +0300

    add MUSCL limiter

--- limiters/MUSCLLimiter.cpp
+++ limiters/MUSCLLimiter.cpp
@@ -19,19 +19,32 @@ Polynomial MUSCLLimiter::limit(const Polynomial & uPrev, const Polynomial & u,
 											  const Polynomial & uNext) const
 {
 
+	static const double k = 1;
 	static const double h = m_conditions->getSpatialStep();
+
+	static double j;
+	static double xJ;
+	j = u.getJ();
+	xJ = j * h;
 	//average  values (see limiters definition in article)
-	static double vPrev;
-	static double v;
-	static double vNext;
+	static double avgValuePrev;
+	static double avgValue;
+	static double avgValueNext;
 
-//	vPrev = calcAvgValue(m_k, jPrev, h, uPrev);
-//	v = calcAvgValue(m_k, j, h, u);
-//	vNext = calcAvgValue(m_k, jNext, h, uPrev);
+	avgValuePrev = calcAvgValue(h, uPrev);
+	avgValue = calcAvgValue(h, u);
+	avgValueNext = calcAvgValue(h, uNext);
 
-//	const int size = m_k + 1;
-//	double limitedCoefs [size];
-//	limited
-	return u;
+	Polynomial uNew(j, k);
 
+	static double b;
+	static double c;
+	b = (avgValueNext - avgValue) / h;
+	c = (avgValue - avgValuePrev) / h;
+
+	// just like in the article
+	uNew(0) = avgValue - xJ * m(u(1), b, c);
+	uNew(1) = m(u(1), b, c);
+
+	return uNew;
 }
